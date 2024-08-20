4f91b5b23c948db7161569242cd1cebef3e282a1
Damien Savatier <damien.savatier@visian.fr>
2024-08-20 02:07:36 +0200

    Escape _ in webhook audit

--- app/services/audit.py
+++ app/services/audit.py
@@ -21,7 +21,10 @@ def log(*args, level=logging.INFO, codeblock=None, **kwargs):
     if not config.AUDIT_WEBHOOK:
         return
 
-    webhook_msg = "\n".join(items).replace("`", "\\`")
+    webhook_msg = "\n".join(items)
+    webhook_msg = webhook_msg.replace("`", "\\`")
+    webhook_msg = webhook_msg.replace("_", "\\_")
+
     if codeblock:
         webhook_msg_codeblock = f"{webhook_msg}\n```\n{codeblock}\n```"
     else:
