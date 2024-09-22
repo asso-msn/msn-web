d3f88c5be86f8740d3db70fed9f6ac8517c1edb7
Tina <me@tina.moe>
2024-09-22 22:53:19 +0200

    Update pdf poster CLI to run on all games when no args

--- app/cli/pdf.py
+++ app/cli/pdf.py
@@ -17,13 +17,18 @@ def group():
 @click.argument("slug", default="")
 def poster(slug):
     """Generate a game info poster."""
-    game = games.get(slug)
-    if not game:
-        raise click.UsageError(
-            f"Invalid game. Choose from: {', '.join(games.get_slugs())}."
-        )
-    OUTPUT_DIR.mkdir(exist_ok=True)
-    dest = OUTPUT_DIR / f"{slug}.pdf"
-    html = app.render("pdf/game_info_poster", game=game)
-    pdf.save_html(html, dest)
-    click.echo(f"Generated game info poster at `{dest}`.")
+    if not slug:
+        games_ = [game for game in app.data["games_posters"]]
+    else:
+        games_ = [slug]
+    for slug in games_:
+        game = games.get(slug)
+        if not game:
+            raise click.UsageError(
+                f"Invalid game. Choose from: {', '.join(games.get_slugs())}."
+            )
+        OUTPUT_DIR.mkdir(exist_ok=True)
+        dest = OUTPUT_DIR / f"{slug}.pdf"
+        html = app.render("pdf/game_info_poster", game=game)
+        pdf.save_html(html, dest)
+        click.echo(f"Generated game info poster at `{dest}`.")
