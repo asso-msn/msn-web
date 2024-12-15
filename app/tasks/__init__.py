659b308be5e92d67f268a96f2feaa4eae58f991a
Tina <skielred@gmail.com>
2024-12-15 23:18:13 +0100

    Print tasks statuses instead of logging to audit log

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -15,11 +15,11 @@ def descript_task(f):
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
         start = time.monotonic()
-        audit.log(f"Task started: {f.__name__}", start=datetime.now())
+        print(f"Task started: {f.__name__}")
         f(*args, **kwargs)
         delta = time.monotonic() - start
         minutes, seconds = int(delta // 60), int(delta % 60)
         duration = f"{minutes}m {seconds}s"
-        audit.log(f"Task finished: {f.__name__}", duration=duration)
+        print(f"Task finished: {f.__name__}, {duration} elapsed")
 
     return wrapper
