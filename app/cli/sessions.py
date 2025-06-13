d57957c327a63e6960a359b3a04ffccd3aa930a4
Tina <me@tina.moe>
2025-06-13 14:59:35 +0200

    Add sessions clear cli

---
+++ app/cli/sessions.py
@@ -0,0 +1,12 @@
+from app import app
+
+
+@app.cli.group()
+def sessions():
+    pass
+
+@sessions.command()
+def clear():
+    with app.session() as s:
+        s.query(app.session_interface.sql_session_model).delete()
+        s.commit()
