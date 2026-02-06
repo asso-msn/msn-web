5e6f017be45e5cf482dc3297de6f1407dc9122f5
Tina <me@tina.moe>
2026-02-06 22:07:38 +0100

    Add game filter to members map

--- app/routes/map.py
+++ app/routes/map.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 
 import flask
+from wtforms import SelectField
 
 from app import app
-from app.db import User
-from app.services import user
+from app.db import User, UserGame
+from app.forms import Form
+from app.services import games, user
 
 
 @dataclass
@@ -18,12 +20,23 @@ class UserPoint:
     link: str
 
 
+class MapForm(Form):
+    game = SelectField(
+        choices=[("", "Tous")] + [(x.slug, x.name) for x in games.get_all()]
+    )
+
+
 @app.get("/map/")
 def map():
+    game = flask.request.args.get("game")
     with app.session() as s:
         query = s.query(User)
         user.filter_public(query)
         query = query.filter(User.map_point_id.isnot(None))
+        if game and (game := games.get(game)):
+            query = query.filter(
+                User.games.any(UserGame.game.has(slug=game.slug))
+            )
         user_points = [
             UserPoint(
                 latitude=user.map_point.latitude,
@@ -36,6 +49,9 @@ def map():
             )
             for user in query
         ]
+    form = MapForm()
+    if game:
+        form.game.data = game.slug
     return app.render(
-        "map", user_points=user_points, title="La carte des membres"
+        "map", user_points=user_points, title="La carte des membres", form=form
     )
